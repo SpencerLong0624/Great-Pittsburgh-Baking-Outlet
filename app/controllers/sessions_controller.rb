a4fe6393fe044e484c23b284bb1cab46c8809f5b
Spencer Long <spencerlong0624@gmail.com>
2022-04-01 02:16:57 -0400

    Finished sessions controller for ck1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  include AppHelpers::Cart
+  def create
+    user = User.authenticate(params[:username], params[:password])
+    if user
+      session[:user_id] = user.id
+      session[:cart] = create_cart
+      redirect_to home_path, notice: "Logged in!"
+    else
+      flash.now.alert = "Username and/or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    destroy_cart
+    redirect_to home_path, notice: "Logged out!"
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,17 @@ Rails.application.routes.draw do
   # get 'home/about'
   # get 'home/contact'
 
+  #Home Controller Routes
   get 'home', to: 'home#index', as: :home
   get 'home/privacy', to: 'home#privacy', as: :privacy
   get 'home/about', to: 'home#about', as: :about
   get 'home/contact', to: 'home#contact', as: :contact
   # get 'home/search', to: 'home#search', as: :search
 
+  #Session Controller Routes
+  resources :sessions, only: [:create]
+  get 'login', to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
 
   # API routing
   scope module: 'api', defaults: {format: 'json'} do
