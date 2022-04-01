f54b2d419445951b2146cbfc805c9cb5629c0f28
Spencer Long <spencerlong0624@gmail.com>
2022-04-01 02:02:24 -0400

    Finished Home Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  # get 'home/privacy'
+  # get 'home/about'
+  # get 'home/contact'
+
+  get 'home', to: 'home#index', as: :home
+  get 'home/privacy', to: 'home#privacy', as: :privacy
+  get 'home/about', to: 'home#about', as: :about
+  get 'home/contact', to: 'home#contact', as: :contact
+  # get 'home/search', to: 'home#search', as: :search
+
+
   # API routing
   scope module: 'api', defaults: {format: 'json'} do
     namespace :v1 do
